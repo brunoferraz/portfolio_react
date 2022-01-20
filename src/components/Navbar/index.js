63c36f9618b45d36200f2ac9a3f882fa1f175ff3
Bruno Ferraz <brunoferraz.pro@gmail.com>
2022-01-20 07:58:18 -0300

    [2]sass problem solved

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,18 @@
 import React, { Fragment } from "react";
-import "./style.scss"
-
 import { Link } from "react-router-dom";
+import './style.scss';
 
 const Navbar = ()=>{
     return(
         <Fragment>
             <header className="header">
-                <div>Brand</div>
-                <nav>
+                <div className="brand"></div>
+                {/* <img alt="Meu avatar" src={"./assets/avatar.png"}/> */}
+                <h1>Bruno Ferraz</h1>
+                <nav className="navBar">
                     <Link to="/about">About</Link>
-                    <Link to="/contact">About</Link>
+                    <Link to="/contact">Portfolio</Link>
+                    <Link to="/contact">Contact</Link>
                 </nav>
             </header>
         </Fragment>
