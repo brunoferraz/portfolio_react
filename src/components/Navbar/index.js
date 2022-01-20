31c3fffedbaa3654404663cbfd633369aa4596cd
Bruno Ferraz <brunoferraz.pro@gmail.com>
2022-01-20 18:20:32 -0300

    [2]navbar arranged. Now interaction with nav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -9,11 +9,13 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { useMediaQuery } from "react-responsive";
 
 const barFolded = " barFolded";
-const barOpened = " barOpened";
+const barUnfolded = " barUnfolded";
 
 const Navbar = ()=>{
     const [barState, setBarFoldState] = useState(barFolded); 
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+    let screenQuery = "";
+    if(isTabletOrMobile) screenQuery = " mobile"
 
     // const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 1224px)'})
     // const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
@@ -21,17 +23,15 @@ const Navbar = ()=>{
     // const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
 
     const controlBar =()=>{
-        if(barState === barOpened){
+        if(barState === barUnfolded){
             setBarFoldState(barFolded);
         }else{
-            setBarFoldState(barOpened);
+            setBarFoldState(barUnfolded);
         }
     }
 
-
     console.log(isTabletOrMobile);
-    let screenQuery = "";
-    if(isTabletOrMobile) screenQuery = " mobile"
+
     let burgermenu = ()=>{
         if(!isTabletOrMobile){ return null;}
         return(
@@ -44,22 +44,22 @@ const Navbar = ()=>{
     }
     return(
         <Fragment>
-            <header className={"header"} id="navbarToggleExternalContent">
-                <img className={"brand"} alt="Meu avatar" src={"./assets/avatar.png"}/>
-                <div class={"title"}>Bruno Ferraz</div>
-                <nav className={"navBar"}>
+            <header className={"header"+screenQuery+barState} id="navbarToggleExternalContent">
+                <img className={"brand"+screenQuery} alt="Meu avatar" src={"./assets/avatar.png"}/>
+                <div class={"title"+screenQuery}>Bruno Ferraz</div>
+                
+                <nav className={"navBar"+screenQuery}>
                     <Link to="/about">About</Link>
                     <Link to="/contact">Portfolio</Link>
                     <Link to="/contact">Contact</Link>
                 </nav>
-                {burgermenu()}
-                <div className={"socialNetworkBar"}>
+                <div className={"socialNetworkBar"+screenQuery+barState}>
                     <a href="https://www.facebook.com/bruno.ferraz.585/"><FontAwesomeIcon icon={faFacebookF} /></a>
                     <a href="https://www.behance.net/brunoferrazpro"><FontAwesomeIcon icon={faBehance} /></a>
                     <a href="https://www.instagram.com/zarrefrb/"><FontAwesomeIcon icon={faInstagram}/></a>
                     <a href="https://github.com/brunoferraz"><FontAwesomeIcon icon={faGithub}/></a>
                 </div>
-                
+                {burgermenu()}
             </header>
         </Fragment>
     )
