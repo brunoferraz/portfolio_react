a9af96f8c2360743fc4515d45fa4b64c04cd04cd
brunoferraz <brunoferraz.pro@gmail.com>
2022-02-08 23:01:32 -0300

    [2]project detail loading hiw own file

--- src/components/Portfolio/ProjectDetail/index.js
+++ src/components/Portfolio/ProjectDetail/index.js
@@ -9,11 +9,6 @@ import Description from "./shared/Description/index.js";
 import GoBackButton from "../../shared/GoBackButton/index.js";
 
 
-async function getPortfolio(){
-    let response = await fetch("./../api/mock-projects.json");
-    let data = await response.json();
-    return data;
-}
 async function getProject(projid){
     let response = await fetch(`./../api/${projid}.json`);
     let data = await response.json();
@@ -28,35 +23,14 @@ const ProjectDetail = (props)=>{
     let history = useHistory();
 
     useEffect(() =>{
-            // console.log(portfoliodata)
-        if(portfoliodata.length===0){
-            getPortfolio().then((data)=>{
-                setPortfolioData(data["projects"])
-                if(id>portfoliodata.length || isNaN(id)){
-                    //baixou os dados mas nao tem o projeto solicitado
-                    setRedirect(true);
-                }
-            }, error=>{
-                setRedirect(true);
-            }
-            )
-        }else{
-            
-            setCurrentProject(portfoliodata[id])
-            getTags([...portfoliodata[id].tags])
-        }
+        getProject(id).then((data)=>{
+            // console.log(data.tags)
+            setCurrentProject(data)
+            getTags([...data.tags])
+        }, err=>{
+            setRedirect(true);
+        })
     },[id])
-    useEffect(() =>{
-        if(portfoliodata.length!==0){
-            if(id>portfoliodata.length || isNaN(id)){
-                //baixou os dados mas nao tem o projeto solicitado
-                setRedirect(true);
-            }else{
-                setCurrentProject(portfoliodata[id])
-                getTags([...portfoliodata[id].tags])
-            }
-        }
-    },[portfoliodata])
     // let path = useRecoilValue(absolutPath);
     let path = "./../"
     const goBack = ()=>{
@@ -78,15 +52,8 @@ const ProjectDetail = (props)=>{
                         <div key={index} className="project_tag">{tag}</div>
                         )}
                 </div>
-                {/* {console.log(containsHTML(currentProject.description))} */}
                 <Description className="project_description" str={currentProject.description} />
-                {/* <div className="project_description">{currentProject.description}</div> */}
                 {!currentProject.post?null:<PostComponent id={id} screenQuery={props.screenQuery} />}
-                {/* <div className="project_backButton">
-                    <div onClick={goBack}>
-                    back
-                    </div>
-                </div> */}
                 <GoBackButton></GoBackButton>
             </div>
             }
